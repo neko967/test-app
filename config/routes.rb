5b8131bd6c5e183d59064379233a85946f559cc6
neko967 <113199838+neko967@users.noreply.github.com>
2023-12-27 17:20:26 +0900

    topを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'staticpages/top'
+  root "staticpages#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
